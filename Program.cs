cce8c9d894620c84460936f5960924aff4be1755
Igor Puchkov <sibmost1@ngs.ru>
2022-09-17 17:07:51 +0700

    Task34

--- Program.cs
+++ Program.cs
@@ -1,34 +1,38 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // 345, 897, 568, 234] -> 2
 
-int []GenerateRandomArray (int size, int start, int end)
-    {
-        int RandomArray = new int[size];
-        for (int i = 0; i< size; i++)
-        {
-            RandomArray[i] = new Random().Next(start, end+1);
-            return RandomArray;
-        }
-        void PrintArray(int[] Arrey1)
-        {
-            for (int i = 0; i < Array1.Length; i++)
+int[] GenerateRandomArray(int size, int start, int end)
             {
-                Console.WriteLine(Convert.ToString(Arrey1[i]));
-            }
-        }
-        int CountEven(int[] Array2)
-        {
-            int count = 0;
-            for (int =0; i < Array2.Length; i++)
-                if (Array2[i] % 2 ==0)
+                int[] RandomArray = new int[size];
+                for (int i = 0; i < size; i++)
                 {
-                    count = count + 1;
+                    RandomArray[i] = new Random().Next(start, end + 1);
                 }
-            return count;    
-        }
+                return RandomArray;
+            }
 
-        int[] Array = new int[5];
-        Array = GeneratedRandomArray(5, 100, 999);
-        PrintArray(Array);
-        Console.WriteLine("количество четных чисел массива" + CountofEven (Array));
-    }
+            void PrintArray(int[] Array1)
+            {
+                for (int i = 0; i < Array1.Length; i++)
+                {
+                    Console.WriteLine(Convert.ToString(Array1[i]));
+                }
+            }
+
+            int CountOfEven(int[] Array2)
+            {
+                int Count = 0;
+                for (int i = 0; i < Array2.Length; i++)
+                    if (Array2[i] % 2 == 0)
+                    {
+                        Count = Count + 1;
+                    }
+                return Count;
+            }
+
+            int[] Array = new int[4];
+            Array = GenerateRandomArray(4, 100, 999);
+            PrintArray(Array);
+            Console.WriteLine("количество четных чисел массива " + CountOfEven(Array));
+           
+  
